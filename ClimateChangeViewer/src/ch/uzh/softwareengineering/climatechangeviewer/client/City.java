0ba092f66a15150cf2636f2e34bf5639c5d6152f
cryptonauticus <nimbatus@posteo.de>
2016-11-08 20:57:01 +0100

    Projekt-Neustart 2

---
+++ ClimateChangeViewer/src/ch/uzh/softwareengineering/climatechangeviewer/client/City.java
@@ -0,0 +1,57 @@
+package ch.uzh.softwareengineering.climatechangeviewer.client;
+
+import java.io.Serializable;
+
+public class City implements Serializable  {
+	
+	private String date;
+	private String cityName;
+	private String country;
+	private String averageTemperatureUncertainty;
+	private String averageTemperature;
+	private String longitude;
+	private String latitude;
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAverageTemperatureUncertainty() {
+		return averageTemperatureUncertainty;
+	}
+	public void setAverageTemperatureUncertainty(String averageTemperatureUncertainty) {
+		this.averageTemperatureUncertainty = averageTemperatureUncertainty;
+	}
+	public String getAverageTemperatute() {
+		return averageTemperature;
+	}
+	public void setAverageTemperature(String averageTemperatute) {
+		this.averageTemperature = averageTemperatute;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String langitude) {
+		this.longitude = langitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}	
+}
---
+++ ClimateChangeViewer/src/ch/uzh/softwareengineering/climatechangeviewer/client/Filter.java
@@ -0,0 +1,33 @@
+package ch.uzh.softwareengineering.climatechangeviewer.client;
+
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+
+public class Filter {
+	
+	private TextBox filterBox = new TextBox();
+	private String value;
+	private Label label = new Label("Filter");
+	private HorizontalPanel filterPanel = new HorizontalPanel();
+	
+	public TextBox getTextBox() {
+		return filterBox;		
+	}
+	
+	public HorizontalPanel getPanel() {
+		filterPanel.add(filterBox);
+		filterPanel.add(label);
+		return filterPanel;
+	}
+	public void setValue(String s) {
+		value = s;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+
+}
