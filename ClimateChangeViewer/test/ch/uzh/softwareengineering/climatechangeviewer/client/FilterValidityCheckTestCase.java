ce9a51791242520cbf1d1c7af177c3b91186ca1b
cryptonauticus <nimbatus@posteo.de>
2016-11-13 23:37:50 +0100

    Testumgebung eingerichtet.

---
+++ ClimateChangeViewer/test/ch/uzh/softwareengineering/climatechangeviewer/client/FilterValidityCheckTestCase.java
@@ -0,0 +1,14 @@
+package ch.uzh.softwareengineering.climatechangeviewer.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FilterValidityCheckTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
