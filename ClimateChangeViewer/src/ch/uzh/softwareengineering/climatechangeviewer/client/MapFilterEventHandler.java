1ecd1c8d5728094929f4838d575df7e04db4b30b
cryptonauticus <nimbatus@posteo.de>
2016-12-11 15:29:46 +0100

    Tooltip-Offset angepasst.

--- ClimateChangeViewer/src/ch/uzh/softwareengineering/climatechangeviewer/client/MapFilterEventHandler.java
+++ ClimateChangeViewer/src/ch/uzh/softwareengineering/climatechangeviewer/client/MapFilterEventHandler.java
@@ -14,46 +14,53 @@ import com.google.gwt.user.client.ui.Widget;
 
 import ch.uzh.softwareengineering.climatechangeviewer.client.widget.slider.SliderEvent;
 import ch.uzh.softwareengineering.climatechangeviewer.client.widget.slider.SliderListener;
+import ch.uzh.softwareengineering.climatechangeviewer.server.QueryServiceImpl;
 
-public class MapFilterEventHandler extends Composite implements KeyDownHandler, MouseOverHandler, MouseOutHandler, SliderListener {
-	
+public class MapFilterEventHandler extends Composite implements KeyDownHandler, MouseOverHandler, MouseOutHandler,
+		SliderListener {
+
 	public static final int COMPARISON_PERIOD_LENGTH = MapView.COMPARISON_PERIOD_LENGTH;
-	public static final int TOOLTIP_OFFSET_Y = 70;
-	
+	public static final int TOOLTIP_OFFSET_Y = 80;
+
 	private MapView mapView;
-	
-	private FilterTooltip period1QueryTooltip = new FilterTooltip("Choose the starting year (min. 1745) of the earlier period (=10 year range) that shall be compared.");
-	private FilterTooltip period2QueryTooltip = new FilterTooltip("Choose the starting year (max. 2003) of the later period (=10 year range) that shall be compared.");
-	private FilterTooltip uncertaintyQueryTooltip = new FilterTooltip("If entered, the data shown is restricted to the maximum average uncertainty of your choice.");
-	
+
+	private FilterTooltip period1QueryTooltip = new FilterTooltip("Choose the starting year "
+			+ "(min. " + QueryServiceImpl.OLDEST_YEAR_IN_DATAFILE + ") of the earlier period (range = 10 years) "
+			+ "that shall be compared.");
+	private FilterTooltip period2QueryTooltip = new FilterTooltip("Choose the starting year "
+			+ "(max. " + (QueryServiceImpl.LATEST_YEAR_IN_DATAFILE - COMPARISON_PERIOD_LENGTH + 1) + ") "
+			+ "of the later period (range = 10 years) that shall be compared.");
+	private FilterTooltip uncertaintyQueryTooltip = new FilterTooltip("If entered, the data shown is restricted to "
+			+ "the maximum average uncertainty of your choice.");
+
 	private DoubleBox uncertaintyQueryInputBox;
-	
+
 	private Label period1QueryLabel;
 	private Label period2QueryLabel;
 	private Label period1QueryValueLabel;
 	private Label period2QueryValueLabel;
 	private Label uncertaintyQueryLabel;
-	
+
 	public MapFilterEventHandler(MapFilter filter, MapView mapView) {
 		this.mapView = mapView;
-		
+
 		// Adding MouseOverHandler and MouseOutHandler for the Labels.
 		period1QueryLabel = filter.getPeriod1QueryLabel();
 		period1QueryLabel.addMouseOverHandler(this);
 		period1QueryLabel.addMouseOutHandler(this);
-		
+
 		period2QueryLabel = filter.getPeriod2QueryLabel();
 		period2QueryLabel.addMouseOverHandler(this);
 		period2QueryLabel.addMouseOutHandler(this);
-		
+
 		period1QueryValueLabel = filter.getPeriod1QueryValueLabel();
 		period1QueryValueLabel.addMouseOverHandler(this);
 		period1QueryValueLabel.addMouseOutHandler(this);
-		
+
 		period2QueryValueLabel = filter.getPeriod2QueryValueLabel();
 		period2QueryValueLabel.addMouseOverHandler(this);
 		period2QueryValueLabel.addMouseOutHandler(this);
-		
+
 		uncertaintyQueryLabel = filter.getUncertaintyQueryLabel();
 		uncertaintyQueryLabel.addMouseOverHandler(this);
 		uncertaintyQueryLabel.addMouseOutHandler(this);
@@ -61,29 +68,31 @@ public class MapFilterEventHandler extends Composite implements KeyDownHandler, 
 		//Adding KeyDownHandler to the text boxes.
 		uncertaintyQueryInputBox = filter.getUncertaintyQueryInputBox();
 		uncertaintyQueryInputBox.addKeyDownHandler(this);
-		
+
 	}
-	
+
 	@Override
 	public void onMouseOver(MouseOverEvent event) {
 		Widget sender = (Widget) event.getSource();
-		
+
 		if(sender == period1QueryLabel) {
 			period1QueryTooltip.setPopupPosition(event.getClientX(), event.getClientY() - TOOLTIP_OFFSET_Y);
 			period1QueryTooltip.show();
+
 		} else if(sender == period2QueryLabel) {
 			period2QueryTooltip.setPopupPosition(event.getClientX(), event.getClientY() - TOOLTIP_OFFSET_Y);
 			period2QueryTooltip.show();
+
 		} else if(sender == period1QueryValueLabel) {
 			period1QueryTooltip.setPopupPosition(event.getClientX(), event.getClientY() - TOOLTIP_OFFSET_Y);
 			period1QueryTooltip.show();
+
 		} else if(sender == period2QueryValueLabel) {
 			period2QueryTooltip.setPopupPosition(event.getClientX(), event.getClientY() - TOOLTIP_OFFSET_Y);
 			period2QueryTooltip.show();
+
 		} else if(sender == uncertaintyQueryLabel) {
 			uncertaintyQueryTooltip.setPopupPosition(event.getClientX(), event.getClientY() - TOOLTIP_OFFSET_Y);
-			uncertaintyQueryTooltip.show();
-		} else if(sender == uncertaintyQueryTooltip) {
 			uncertaintyQueryTooltip.show();
 		}
 	}
@@ -108,34 +117,36 @@ public class MapFilterEventHandler extends Composite implements KeyDownHandler, 
 	@Override
 	public void onKeyDown(KeyDownEvent event) {
 		Widget sender = (Widget) event.getSource();
-		
+
 		if(sender == uncertaintyQueryInputBox) {
 			if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
 				mapView.getMapData();
 			}
 		}
 	}
-	
-    @Override
-    public void onChange(SliderEvent event) {
-        // We don't need to do anything, because everything is done in onSlide.
-    }
 
-    @Override
-    public boolean onSlide(SliderEvent event) {
-        period1QueryValueLabel.setText(event.getValues()[0] + " - " + (event.getValues()[0] + COMPARISON_PERIOD_LENGTH - 1));
-        period2QueryValueLabel.setText(event.getValues()[1] + " - " + (event.getValues()[1] + COMPARISON_PERIOD_LENGTH - 1));
-        return true;
-    }
+	@Override
+	public void onChange(SliderEvent event) {
+		// We don't need to do anything, because everything is done in onSlide.
+	}
 
-    @Override
-    public void onStart(SliderEvent event) {
-        // We are not going to do anything onStart.
-    }
+	@Override
+	public boolean onSlide(SliderEvent event) {
+		period1QueryValueLabel.setText(event.getValues()[0] + " - " + (event.getValues()[0]
+				+ COMPARISON_PERIOD_LENGTH - 1));
+		period2QueryValueLabel.setText(event.getValues()[1] + " - " + (event.getValues()[1]
+				+ COMPARISON_PERIOD_LENGTH - 1));
+		return true;
+	}
 
-    @Override
-    public void onStop(SliderEvent event) {
-        // We are not going to do anything onStop.      
-    }
-	
+	@Override
+	public void onStart(SliderEvent event) {
+		// We are not going to do anything onStart.
+	}
+
+	@Override
+	public void onStop(SliderEvent event) {
+		// We are not going to do anything onStop.      
+	}
+
 }
--- ClimateChangeViewer/src/ch/uzh/softwareengineering/climatechangeviewer/client/TableFilterEventHandler.java
+++ ClimateChangeViewer/src/ch/uzh/softwareengineering/climatechangeviewer/client/TableFilterEventHandler.java
@@ -17,19 +17,25 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class TableFilterEventHandler extends Composite implements KeyDownHandler, MouseOverHandler, MouseOutHandler {
 	
-	public static final int TOOLTIP_OFFSET_Y = 70;
+	public static final int TOOLTIP_OFFSET_Y = 80;
 	
 	private TableView tableView;
 
-	private FilterTooltip popupMonth = new FilterTooltip("If you choose a month from"
-			+ " the drop-down menu, only data for that month will be shown.");
-	private FilterTooltip year1QueryToolTip = new FilterTooltip("Only data starting from the year entered here will be shown.");
-	private FilterTooltip year2QueryToolTip = new FilterTooltip("Only data earlier than or equal to the year entered here will be shown.");
-	private FilterTooltip countryQueryToolTip = new FilterTooltip("Restrict the data shown to a country of your choice.");
+	private FilterTooltip popupMonth = new FilterTooltip("If you choose a month from "
+			+ "the drop-down menu, only data for that month will be shown.");
+	private FilterTooltip year1QueryToolTip = new FilterTooltip("Only data starting from the year entered here will "
+			+ "be shown.");
+	private FilterTooltip year2QueryToolTip = new FilterTooltip("Only data earlier than or equal to the year entered "
+			+ "here will be shown.");
+	private FilterTooltip countryQueryToolTip = new FilterTooltip("Restrict the data shown to a country of your "
+			+ "choice.");
 	private FilterTooltip cityQueryToolTip = new FilterTooltip("Restrict the data shown to a city of your choice.");
-	private FilterTooltip minTemperatureQueryToolTip = new FilterTooltip("Only data starting from the temperature entered will be shown.");
-	private FilterTooltip maxTemperatureQueryToolTip = new FilterTooltip("Only data lower than or equal to the temperature entered here will be shown.");
-	private FilterTooltip uncertaintyQueryToolTip = new FilterTooltip("If entered, the data shown is restricted to the maximum average uncertainty of your choice.");
+	private FilterTooltip minTemperatureQueryToolTip = new FilterTooltip("Only data starting from the temperature "
+			+ "entered will be shown.");
+	private FilterTooltip maxTemperatureQueryToolTip = new FilterTooltip("Only data lower than or equal to the "
+			+ "temperature entered here will be shown.");
+	private FilterTooltip uncertaintyQueryToolTip = new FilterTooltip("If entered, the data shown is restricted to the "
+			+ "maximum average uncertainty of your choice.");
 	
 	private Label cityQueryLabel;
 	private Label countryQueryLabel;
